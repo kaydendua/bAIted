ba25bc9e2fd9073e760e1122c372d8562eb38c41
jarelland-shouwa <songjarell@gmail.com>
2026-01-17 22:50:06 +0800

    Added Vote button

--- app/discussion/page.tsx
+++ app/discussion/page.tsx
@@ -1,5 +1,6 @@
 import PlayerCodeCarousel from "@/components/ui/playerCodeDisplay";
 import TopBar from "@/components/ui/topbar";
+import { Button } from "@/components/ui/button"
 
 export default function WebIDEPage() {
   return (
@@ -7,6 +8,7 @@ export default function WebIDEPage() {
       <div className="flex flex-col justify-center items-center">
         <TopBar></TopBar>
         <div className="">DISCUSS!</div>
+        <Button className="w-2xl">Vote</Button>
         <PlayerCodeCarousel></PlayerCodeCarousel>
       </div>
     </>
