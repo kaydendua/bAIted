8830ec4f8de8574baaad9015af3f1c9683fafce8
unset <ddcro07@gmail.com>
2026-01-17 16:27:20 +0800

    added codemirror editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Editor from "./web-ide/ide";
+
 export default function Home() {
   return (
-    <div>bAIted</div>
+    <div>
+    bAIted
+    <Editor />
+    </div>
   );
 }
---
+++ app/web-ide/ide.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useState } from "react";
+import CodeMirror, { oneDark } from "@uiw/react-codemirror";
+import { python } from "@codemirror/lang-python";
+import { indentUnit } from "@codemirror/language";
+
+export default function Editor() {
+  const [code, setCode] = useState("// Code");
+
+  return (
+    <CodeMirror
+      value={code}
+      height="300px"
+      extensions={[python(), indentUnit.of("    ")]}
+      theme={oneDark}
+      onChange={(value) => setCode(value)}
+    />
+  );
+}
