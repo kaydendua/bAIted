982c2743ff0dda7ba49d7e8c5c9f5bfda7963349
Felix Dimjati <100569717+felixcodesstuff@users.noreply.github.com>
2026-01-18 09:14:44 +0800

    improved homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,13 @@ export default function Home() {
   return (
     <LobbyProvider>
       <div className="w-full">
-        <header className="flex flex-col items-center">
+        <header className="flex flex-col items-center mb-12">
           <img src='/logo.png' alt="Logo" className="h-48 w-96 object-contain"/>
-          <p className="text-lg text-balance opacity-80 sm:text-xl">
-            A spin-off on Among Us. A fun game where players code answers to questions and attempt determine who is the Vibe coder.
+          <p className="text-lg text-balance text-red-50 sm:text-xl">
+            bAIted is a spin-off on Among Us!
+          </p>
+          <p className="text-lg text-balance text-red-50 sm:text-xl">
+            Players code answers to questions and attempt determine who is the <span className="text-red-500 font-semibold">Vibe Coder</span>.
           </p>
         </header>
         <div className="w-full h-full flex items-center justify-center">
